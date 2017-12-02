b207cca18de50949a95ea18969d24c82be209df1
sina- <sina-@address.com>
2017-12-02 18:00:57 +0100

    use istreambuf_iterator for reading files

--- granite-core/src/utils/file.cpp
+++ granite-core/src/utils/file.cpp
@@ -2,16 +2,9 @@
 
 std::string granite::read_file(const char* path)
 {
-	std::string result("");
-	std::ifstream file(path);
-    std::string str; 
-    while (std::getline(file, str))
-    {
-		result += str;
-		result.push_back('\n');
-    }
-
-	if (result.size() == 0)
-		throw std::runtime_error("File not found " + std::string(path));
-	return result;
+	std::ifstream ifs(path);
+	std::string content(std::istreambuf_iterator<char>(ifs.rdbuf()), std::istreambuf_iterator<char>());
+	if (content.size() == 0)
+		throw std::runtime_error("Empty file " + std::string(path));
+	return content;
 }

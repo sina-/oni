0172c045930758f0ff4efd7a4353f3cf46fd5f5f
sina- <sina-@address.com>
2017-12-17 20:49:08 +0100

    using simple2drenderer

--- granite-core/main.cpp
+++ granite-core/main.cpp
@@ -6,6 +6,9 @@
 #include "src/math/math.h"
 #include "src/graphics/buffers/indexbuffer.h"
 #include "src/graphics/buffers/vertexarray.h"
+#include "src/graphics/renderable2d.h"
+#include "src/graphics/simple2drenderer.h"
+#include "src/graphics/renderer2d.h"
 
 
 int main() {
@@ -17,36 +20,18 @@ int main() {
 
 	Window window("Granite", 800, 600);
 
-	const std::vector<GLfloat>vertices = {
-		0, 0, 0,
-		0, 3, 0,
-		8, 3, 0,
-		8, 0, 0
-	};
-	const std::vector<GLushort> indices = {
-		0, 1, 2,
-		2, 3, 0
-	};
-	std::shared_ptr<Buffer> vbo = std::make_shared<Buffer>(vertices, 3);
-	IndexBuffer ibo(indices, 6);
-
-	VertexArray vao;
-	vao.addBuffer(vbo, 0);
-
-	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
-
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 	shader.enable();
-	shader.setUniformMat4("pr_matrix", ortho);
+	shader.setUniformMat4("pr_matrix", mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f));
 	shader.setUniformMat4("vw_matrix", mat4::translation(vec3(2, 2, 0)));
-	shader.setUniformMat4("ml_matrix", mat4::rotation(10, vec3(0, 0, 1)));
+
+	Renderable2D sprite1(math::vec2(4, 4), math::vec3(5, 5, 0), math::vec4(1, 0, 1, 1), shader);
+	Renderable2D sprite2(math::vec2(1, 2), math::vec3(0, 1, 1), math::vec4(0, 1, 1, 1), shader);
+
+	Simple2DRenderer renderer;
 
 	while (!window.closed()) {
 		window.clear();
-		shader.setUniformMat4("ml_matrix", mat4::rotation(window.getCursorX(), vec3(0, 0, 1)));
-		vao.bind();
-		ibo.bind();
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
 
 		/*
 		GLenum err;
@@ -54,9 +39,10 @@ int main() {
 			std::cout << "OpenGL error: " << err << std::endl;
 		}
 		*/
-
-		vao.unbind();
-		ibo.unbind();
+		
+		renderer.submit(&sprite1);
+		renderer.submit(&sprite2);
+		renderer.flush();
 		window.update();
 
 	}

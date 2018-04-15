5a28ad7d431ab4a33ddb657b9589731665d95b49
sina- <sina.tamanna@gmail.com>
2018-04-15 12:02:19 +0200

    forward declare stuff in utils

--- inc/oni-core/utils/file.h
+++ inc/oni-core/utils/file.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <fstream>
 
 namespace oni {
 	std::string read_file(const std::string &path);
--- src/utils/file.cpp
+++ src/utils/file.cpp
@@ -1,9 +1,11 @@
+#include <fstream>
+
 #include <oni-core/utils/file.h>
 
 std::string oni::read_file(const std::string &path) {
     std::ifstream ifs(path.c_str());
     std::string content(std::istreambuf_iterator<char>(ifs.rdbuf()), std::istreambuf_iterator<char>());
-    if (content.size() == 0)
+    if (content.empty())
         throw std::runtime_error("Empty file " + path);
     return content;
 }

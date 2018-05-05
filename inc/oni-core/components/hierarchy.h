1c47f5317cd313b09ac4ab1c9b853c5b005bf60e
sina- <sina.tamanna@gmail.com>
2018-05-05 22:08:20 +0200

    update todo with a possible solution

--- inc/oni-core/components/hierarchy.h
+++ inc/oni-core/components/hierarchy.h
@@ -9,6 +9,9 @@ namespace oni {
         struct TransformParent {
             entities::entityID parent{};
             // TODO: This has to be vector of all the parents transformation matrices.
+            // As it is we can not have grand-parents and such. One way to solve the
+            // problem is to have a map of parentEntity-to-transform and when a parent
+            // transform is updated we only propagate that down the hierarchy.
             math::mat4 transform{math::mat4::identity()};
         };
 

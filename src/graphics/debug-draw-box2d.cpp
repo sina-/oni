9bd9648e20624645a43571d337f50ede95fe7af6
sina- <sina.tamanna@gmail.com>
2018-06-20 21:40:28 +0200

    stub debug draw for box2d

---
+++ src/graphics/debug-draw-box2d.cpp
@@ -0,0 +1,44 @@
+#include <oni-core/graphics/debug-draw-box2d.h>
+
+
+namespace oni {
+    namespace graphics {
+
+        DebugDrawBox2D::DebugDrawBox2D() {
+
+        }
+
+        DebugDrawBox2D::~DebugDrawBox2D() {
+
+        }
+
+        void DebugDrawBox2D::DrawPolygon(const b2Vec2 *vertices, int32 vertexCount, const b2Color &color) {
+
+        }
+
+        void DebugDrawBox2D::DrawSolidPolygon(const b2Vec2 *vertices, int32 vertexCount, const b2Color &color) {
+
+        }
+
+        void DebugDrawBox2D::DrawCircle(const b2Vec2 &center, float32 radius, const b2Color &color) {
+
+        }
+
+        void DebugDrawBox2D::DrawSolidCircle(const b2Vec2 &center, float32 radius, const b2Vec2 &axis,
+                                             const b2Color &color) {
+
+        }
+
+        void DebugDrawBox2D::DrawSegment(const b2Vec2 &p1, const b2Vec2 &p2, const b2Color &color) {
+
+        }
+
+        void DebugDrawBox2D::DrawTransform(const b2Transform &xf) {
+
+        }
+
+        void DebugDrawBox2D::DrawPoint(const b2Vec2 &p, float32 size, const b2Color &color) {
+
+        }
+    }
+}

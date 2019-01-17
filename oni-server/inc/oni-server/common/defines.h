5dae7fc4e6303f8be330ee85c39c8265baf26e79
sina <sina.tamanna@gmail.com>
2019-01-17 17:06:09 +0100

    base each level on the other

--- oni-server/inc/oni-server/common/defines.h
+++ oni-server/inc/oni-server/common/defines.h
@@ -2,18 +2,18 @@
 
 namespace oni {
     namespace server {
-        const oni::common::real32 LEVEL_0 = 0.0f;
-        const oni::common::real32 LEVEL_1 = 0.1f;
-        const oni::common::real32 LEVEL_2 = 0.2f;
-        const oni::common::real32 LEVEL_3 = 0.3f;
-        const oni::common::real32 LEVEL_4 = 0.4f;
-        const oni::common::real32 LEVEL_5 = 0.5f;
-        const oni::common::real32 LEVEL_6 = 0.6f;
-        const oni::common::real32 LEVEL_7 = 0.7f;
-        const oni::common::real32 LEVEL_8 = 0.8f;
-        const oni::common::real32 LEVEL_9 = 0.9f;
-
         const oni::common::real32 MAJOR_LEVEL_DELTA = 0.1f;
         const oni::common::real32 MINOR_LEVEL_DELTA = 0.001f;
+
+        const oni::common::real32 LEVEL_0 = 0.0f;
+        const oni::common::real32 LEVEL_1 = LEVEL_0 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_2 = LEVEL_1 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_3 = LEVEL_2 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_4 = LEVEL_3 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_5 = LEVEL_4 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_6 = LEVEL_5 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_7 = LEVEL_6 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_8 = LEVEL_7 + MAJOR_LEVEL_DELTA;
+        const oni::common::real32 LEVEL_9 = LEVEL_8 + MAJOR_LEVEL_DELTA;
     }
 }

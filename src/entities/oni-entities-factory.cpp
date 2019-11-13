1d7f27893c4b4f084a30f694014e9cc4a6adef8d
sina- <sina.tamanna@gmail.com>
2019-11-13 19:22:55 +0100

    bugfix: don't modify child z when transforming by parent bugfix: worldp3d serialization bug support attachments in json

--- src/entities/oni-entities-factory.cpp
+++ src/entities/oni-entities-factory.cpp
@@ -8,9 +8,25 @@
 #include <oni-core/util/oni-util-enum.h>
 #include <oni-core/json/oni-json.h>
 #include <oni-core/io/oni-io-input-structure.h>
+#include <oni-core/entities/oni-entities-serialization-json.h>
 
 
 namespace {
+    template<class C>
+    C
+    _readComponent(rapidjson::Value &data) {
+        rapidjson::StringBuffer compStringBuff;
+        rapidjson::Writer writer(compStringBuff);
+
+        data.Accept(writer);
+
+        std::istringstream ss(compStringBuff.GetString());
+        cereal::JSONInputArchive reader(ss);
+        auto c = C{};
+        reader(c);
+        return c;
+    }
+
     void
     _createComponent(std::unordered_map<oni::Hash, oni::ComponentFactory> &factoryMap,
                      oni::EntityManager &manager,
@@ -33,8 +49,7 @@ namespace {
         auto factory = factoryMap.find(hash);
         if (factory != factoryMap.end()) {
             // TODO: so many stream and conversions, can't I just use the stream I get from rapidjson?
-            std::istringstream ss;
-            ss.str(compStringBuff.GetString());
+            std::istringstream ss(compStringBuff.GetString());
             cereal::JSONInputArchive reader(ss);
             // TODO: The issue with this right now is that if something is wrong in json I just get
             // an assert! What would happen in publish?
@@ -42,295 +57,6 @@ namespace {
         } else {
             assert(false);
         }
-    }
-}
-
-namespace oni {
-    // TODO: Should I move these into another file that specifies these are only meant for json serialization?
-    // Just like the way I specify network serialization
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              CarConfig &data) {
-        archive("gravity", data.gravity);
-        archive("mass", data.mass);
-        archive("inertialScale", data.inertialScale);
-        archive("halfWidth", data.halfWidth);
-        archive("cgToFront", data.cgToFront);
-        archive("cgToRear", data.cgToRear);
-        archive("cgToFrontAxle", data.cgToFrontAxle);
-        archive("cgToRearAxle", data.cgToRearAxle);
-        archive("cgHeight", data.cgHeight);
-        archive("wheelRadius", data.wheelRadius);
-        archive("wheelWidth", data.wheelWidth);
-        archive("tireGrip", data.tireGrip);
-        archive("lockGrip", data.lockGrip);
-        archive("engineForce", data.engineForce);
-        archive("brakeForce", data.brakeForce);
-        archive("eBrakeForce", data.eBrakeForce);
-        archive("weightTransfer", data.weightTransfer);
-        archive("maxSteer", data.maxSteer);
-        archive("cornerStiffnessFront", data.cornerStiffnessFront);
-        archive("cornerStiffnessRear", data.cornerStiffnessRear);
-        archive("airResist", data.airResist);
-        archive("rollResist", data.rollResist);
-        archive("gearRatio", data.gearRatio);
-        archive("differentialRatio", data.differentialRatio);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Car &data) {
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              WorldP3D &data) {
-        archive("y", data.x);
-        archive("y", data.y);
-        archive("z", data.z);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              WorldP2D &data) {
-        archive("x", data.x);
-        archive("y", data.y);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Orientation &data) {
-        archive("value", data.value);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Scale &data) {
-        archive("x", data.x);
-        archive("y", data.y);
-        archive("z", data.z);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              TimeToLive &data) {
-        archive("currentAge", data.currentAge);
-        archive("maxAge", data.maxAge);
-    }
-
-    template<class Archive>
-    void
-    save(Archive &archive,
-         const Color &data) {
-        auto r = data.r_r32();
-        auto g = data.r_r32();
-        auto b = data.r_r32();
-        auto a = data.r_r32();
-        archive("r", r);
-        archive("g", g);
-        archive("b", b);
-        archive("a", a);
-    }
-
-    template<class Archive>
-    void
-    load(Archive &archive,
-         Color &data) {
-        auto color = vec4();
-        archive("r", color.x);
-        archive("g", color.y);
-        archive("b", color.z);
-        archive("a", color.w);
-        data.set_rgba(color.x, color.y, color.z, color.w);
-    }
-
-    template<class Archive>
-    void
-    save(Archive &archive,
-         const Image &data) {
-        saveHashedString(archive, "name", data.name);
-    }
-
-    template<class Archive>
-    void
-    load(Archive &archive,
-         Image &data) {
-        loadHashedString(archive, "name", data.name);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Texture &data) {
-        archive("image", data.image);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Material_Skin &data) {
-        archive("color", data.color);
-        archive("texture", data.texture);
-    }
-
-    template<class Archive>
-    void
-    save(Archive &archive,
-         const Material_Finish &data) {
-        saveEnum(archive, "name", data);
-    }
-
-    template<class Archive>
-    void
-    load(Archive &archive,
-         Material_Finish &data) {
-        loadEnum(archive, "name", data);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Material_Definition &data) {
-        archive("finish", data.finish);
-        archive("skin", data.skin);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              GrowOverTime &data) {
-        archive("period", data.period);
-        archive("elapsed", data.elapsed);
-        archive("factor", data.factor);
-        archive("maxSize", data.maxSize);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              ParticleEmitter &data) {
-        archive("material", data.material);
-        archive("size", data.size);
-        archive("initialVMin", data.initialVMin);
-        archive("initialVMax", data.initialVMax);
-        archive("orientMin", data.orientMin);
-        archive("orientMax", data.orientMax);
-        archive("acc", data.acc);
-        archive("count", data.count);
-        archive("growth", data.growth);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              AfterMark &data) {
-        archive("material", data.material);
-        archive("type", data.type);
-        archive("size", data.scale);
-    }
-
-    template<class Archive>
-    void
-    save(Archive &archive,
-         const Sound &data) {
-        saveHashedString(archive, "name", data.name);
-        archive("group", data.group);
-    }
-
-    template<class Archive>
-    void
-    load(Archive &archive,
-         Sound &data) {
-        loadHashedString(archive, "name", data.name);
-        archive("group", data.group);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              SoundPitch &data) {
-        archive("value", data.value);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Direction &data) {
-        archive("x", data.x);
-        archive("y", data.y);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Velocity &data) {
-        archive("current", data.current);
-        archive("max", data.max);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              Acceleration &data) {
-        archive("current", data.current);
-        archive("max", data.max);
-    }
-
-    template<class Archive>
-    void
-    save(Archive &archive,
-         const BodyType &data) {
-        saveEnum(archive, "name", data);
-    }
-
-    template<class Archive>
-    void
-    load(Archive &archive,
-         BodyType &data) {
-        loadEnum(archive, "name", data);
-    }
-
-    template<class Archive>
-    void
-    save(Archive &archive,
-         const PhysicalCategory &data) {
-        saveEnum(archive, "name", data);
-    }
-
-    template<class Archive>
-    void
-    load(Archive &archive,
-         PhysicalCategory &data) {
-        loadEnum(archive, "name", data);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              PhysicalProperties &data) {
-        archive("linearDamping", data.linearDamping);
-        archive("angularDamping", data.angularDamping);
-        archive("density", data.density);
-        archive("friction", data.friction);
-        archive("gravityScale", data.gravityScale);
-        archive("highPrecision", data.highPrecision);
-        archive("collisionWithinCat", data.collisionWithinCat);
-        archive("isSensor", data.isSensor);
-        archive("bodyType", data.bodyType);
-        archive("physicalCat", data.physicalCat);
-    }
-
-    template<class Archive>
-    void
-    serialize(Archive &archive,
-              CarInput &data) {
     }
 }
 
@@ -430,14 +156,14 @@ namespace oni {
         // if mapping is missing! Solves so many issues.
         // HMMM WHAT IS A VALID ENTITY NAME? If there is a file matching the requested name, then it must be
         // valid! Not sure if I need to care more than that :/
-        auto id = manager.createEntity(name);
+        auto parentID = manager.createEntity(name);
 
         auto components = document.FindMember("components");
         if (components != document.MemberEnd()) {
             for (auto component = components->value.MemberBegin();
                  component != components->value.MemberEnd(); ++component) {
                 if (component->value.IsObject()) {
-                    _createComponent(mComponentFactory, manager, id, document, component);
+                    _createComponent(mComponentFactory, manager, parentID, document, component);
                 } else {
                     assert(false);
                 }
@@ -445,7 +171,7 @@ namespace oni {
         } else {
             assert(false);
         }
-        _postProcess(manager, id);
+        _postProcess(manager, parentID);
 
         auto entities = document.FindMember("entities");
         if (entities != document.MemberEnd()) {
@@ -455,22 +181,23 @@ namespace oni {
                     auto entityNameObj = entity->value.FindMember("name");
                     if (entityNameObj->value.IsString()) {
                         auto entityName = entityNameObj->value.GetString();
-                        auto _id = createEntity_Canon(manager, {EntityName::makeFromCStr(entityName)});
-                        if (_id == EntityManager::nullEntity()) {
+                        auto childID = createEntity_Canon(manager, {EntityName::makeFromCStr(entityName)});
+                        if (childID == EntityManager::nullEntity()) {
                             assert(false);
                         }
 
-                        // TODO:
-//                        auto attachedObj = entity->value.FindMember("attached");
-//                        if (attachedObj->value.IsObject()) {
-//                            auto attached = attachedObj->value.GetObject();
-//                            if (bindLifetime) {
-//                                oni::EntityManager::bindLifetime({&manager, id}, {&supportEm, _id});
-//                            }
-//                        }
-//                        else {
-//                            assert(false);
-//                        }
+                        auto attached = entity->value.FindMember("attached");
+                        if (attached != entity->value.MemberEnd()) {
+                            if (attached->value.IsObject()) {
+                                auto attachedObj = attached->value.GetObject();
+                                manager.attach(parentID, childID);
+
+                                auto pos = _readComponent<WorldP3D>(attached->value);
+                                manager.setWorldP3D(childID, pos.x, pos.y, pos.z);
+                            } else {
+                                assert(false);
+                            }
+                        }
                     } else {
                         assert(false);
                     }
@@ -480,7 +207,7 @@ namespace oni {
             }
         }
 
-        return id;
+        return parentID;
     }
 
     void
